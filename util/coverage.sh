335bdd3966f4c683575829f757ffd61d5962e8d6
Evelyn Lewis <evelyn@evelynlewis.net>
2024-03-07 01:02:01 -0500

    Add fuzzing code coverage script and CI job

---
+++ util/coverage.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# MIT License
+#
+# Copyright (c) 2024 Evelyn Lewis
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Errors are fatal, and trace execution
+set -ex
+
+# Ensure correct working directory
+test -d ./util/
+test -d ./fuzz/
+
+DEPS="rustfilt cargo-fuzz"
+ARGS="--release --sanitizer none --no-cfg-fuzzing roger"
+
+# Install dependencies
+# shellcheck disable=SC2086
+cargo install --quiet ${DEPS}
+
+# Build fuzz target
+# shellcheck disable=SC2086
+cargo fuzz build ${ARGS}
+
+# Generate coverage
+# shellcheck disable=SC2086
+cargo fuzz coverage ${ARGS} fuzz/corpus/roger/ -- -use_value_profile=1 >/dev/null
+
+# Generate report
+"$(rustc --print=target-libdir)/../bin/llvm-cov" report "$(find target -name roger | grep coverage)" \
+    -Xdemangler=rustfilt \
+    --ignore-filename-regex=.cargo \
+    --ignore-filename-regex='/rustc/.+' \
+    --use-color \
+    -instr-profile=fuzz/coverage/roger/coverage.profdata
+
